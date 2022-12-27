2b32573a96b1e74c4235f2e7615d33b4d415ed73
Fatih Arapoglu <f3tt4n@gmail.com>
2022-12-28 02:27:00 +0300

    add first render function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import { DOM } from "./m-DOM";
 
-// const name = prompt("your name?");
-DOM.initGame(name);
+DOM.initGame("name");
--- src/m-DOM.js
+++ src/m-DOM.js
@@ -10,12 +10,21 @@ class DOM {
         const player = players.human;
         const computer = players.AI;
 
+        player.board.placeShips([0, 0], computer.board.carrier, "vertical");
+        player.board.placeShips([1, 0], computer.board.battleship, "vertical");
+        player.board.placeShips([2, 0], computer.board.destroyer, "vertical");
+        player.board.placeShips([3, 0], computer.board.submarine, "vertical");
+        player.board.placeShips([8, 5], computer.board.patroller, "vertical");
+
         this.placeForAI(computer);
+
+        this.renderGameboardForPlayer(player);
 
         const squares = document.querySelectorAll(".square");
         squares.forEach((square) => square.addEventListener("click", () => {
             const ID = square.dataset.id;
             this.findCoordinates(ID);
+            console.log(ID);
         }));
     };
 
@@ -55,12 +64,38 @@ class DOM {
         computer.board.placeShips([1, 0], computer.board.battleship, "horizontal");
         computer.board.placeShips([2, 0], computer.board.destroyer, "horizontal");
         computer.board.placeShips([3, 0], computer.board.submarine, "horizontal");
-        computer.board.placeShips([4, 0], computer.board.patroller, "horizontal");
-        console.log(computer);
+        computer.board.placeShips([8, 5], computer.board.patroller, "vertical");
     };
 
-    static findCoordinates = (ID) => {
-        console.log(this.coordinatesWithIDs[ID]);
+    static renderGameboardForPlayer = (player) => {
+        const playerBoard = player.board.board;
+        const squares = document.querySelectorAll(".player-square");
+        squares.forEach((square) => {
+            const ID = square.dataset.id;
+            const coordinates = this.findCoordinates(ID);
+            switch (playerBoard[coordinates[0]][coordinates[1]]) {
+            case "water":
+                break;
+            case "Carrier":
+            case "Battleship":
+            case "Destroyer":
+            case "Submarine":
+            case "Patrol Boat":
+            case "hit":
+            case "miss":
+                square.textContent = playerBoard[coordinates[0]][coordinates[1]];
+                break;
+            default:
+                console.log("error");
+            }
+        });
+    };
+
+    static findCoordinates = (ID) => this.coordinatesWithIDs[ID];
+
+    static findID = (coordinates) => {
+        const ID = Object.keys(this.coordinatesWithIDs).find((key) => this.coordinatesWithIDs[key] === coordinates);
+        return ID;
     };
 }
 
